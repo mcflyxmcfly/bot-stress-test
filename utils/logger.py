ead897f7f69e114a819b0e64d1d84f7b5a86d394
mcflyxmcfly <mcflyxmcfly@gmail.com>
2026-05-15 20:14:34 -0300

    Add logger module

---
+++ utils/logger.py
@@ -0,0 +1,13 @@
+"""
+Sistema de logging do bot
+"""
+import logging
+from utils.config import LOG_LEVEL
+
+# Configurar logging
+logging.basicConfig(
+    level=getattr(logging, LOG_LEVEL),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
